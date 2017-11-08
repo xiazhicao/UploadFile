cb0fe480a2ac822e50207293d40273c3e0a545f2
xuzhi <xuzhi@LAPTOP-1HL60N47>
2017-11-08 00:20:58 -0500

    initial version

---
+++ FileUpload/src/main/java/Exception/GlobalDefaultExceptionHandler.java
@@ -0,0 +1,16 @@
+package Exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalDefaultExceptionHandler {
+	
+//	record and output any exception message
+	@ExceptionHandler(value=Exception.class)
+	public String defaultErrorHandler(Exception e, RedirectAttributes redirect) throws Exception {
+			redirect.addFlashAttribute("message", e.getCause().getMessage());
+			return "redirect:/uploadpage";
+	}
+}
